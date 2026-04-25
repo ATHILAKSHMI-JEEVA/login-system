5cdfa8ee3d54da8c45f8982c4643e299b37a4b0f
ATHILAKSHMI-JEEVA <jathilakshmi081@gmail.com>
2026-04-25 17:08:41 +0530

    fix: firebase config initialization error

---
+++ frontend/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDbWDwh20LSMDmXTmQ-eD-VXKQOhSMFgJY",
+  authDomain: "myloginapp-4a42a.firebaseapp.com",
+  projectId: "myloginapp-4a42a",
+  storageBucket: "myloginapp-4a42a.firebasestorage.app",
+  messagingSenderId: "565116600315",
+  appId: "1:565116600315:web:27755f254f7ec000d5155e",
+  measurementId: "G-N9S07Z1469"
+};
+
+window.API_BASE = "https://YOUR_BACKEND_URL_HERE";
