0aed45544e766412c94515df446426369701e721
ATHILAKSHMI-JEEVA <jathilakshmi081@gmail.com>
2026-04-25 18:32:30 +0530

    fix config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,11 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDbWDwh20LSMDmXTmQ-eD-VXKQOhSMFgJY",
-  authDomain: "myloginapp-4a42a.firebaseapp.com",
-  projectId: "myloginapp-4a42a",
-  storageBucket: "myloginapp-4a42a.firebasestorage.app",
-  messagingSenderId: "565116600315",
-  appId: "1:565116600315:web:27755f254f7ec000d5155e",
-  measurementId: "G-N9S07Z1469"
-};
+
 
 window.API_BASE = "https://login-system-backend-i3b8.onrender.com";
